24153ce565259d19f5d8b505cb0cd364e2709f25
giordano19991999@gmail.com <giordano19991999@gmail.com>
2021-11-06 00:09:02 +0100

    piccola evoluzione

--- src/routing_algorithms/ai_routing.py
+++ src/routing_algorithms/ai_routing.py
@@ -31,6 +31,178 @@ class AIRouting(BASE_routing):
     def relay_selection(self, opt_neighbors, pkd):
         """ arg min score  -> geographical approach, take the drone closest to the depot """
 
+        """ arg min score  -> geographical approach, take the drone closest to the depot """
+        
+        #we take our distance from the depot
+        best_drone_distance_from_depot = util.euclidean_distance(self.simulator.depot.coords, self.drone.coords)
+        
+        d = self.drone.next_target()
+        
+        
+        time_taken_best = best_drone_distance_from_depot / self.drone.speed
+        
+        
+       
+        
+        #initially drone closest is us (we take to the depot the
+        #packet without any help)
+        best_drone = None
+
+        #we take all hello packets and all istances of drones
+        for hello_packet, drone_istance in opt_neighbors:
+            
+            """
+            
+                we take the current position, the direction and the speed
+                to estimate next position
+            
+            """
+            
+            """
+            print((drone_istance))
+            
+            print(hello_packet.next_target)
+            
+            print(hello_packet.cur_pos, hello_packet.cur_pos[0])
+            
+            
+            x1 = hello_packet.cur_pos[0]            
+            
+            y1 = hello_packet.cur_pos[1]
+            
+            x2 = hello_packet.next_target[0]
+            
+            y2 = hello_packet.next_target[1]
+            
+            m = (y2 - y1) / (x2 - x1)
+            
+            x_new = x1 + (1 / (np.sqrt(1 + (m*m) ) ) )*0.5
+            
+            y_new = y1 + (m / (np.sqrt(1 + (m*m) ) ) )*0.5
+        
+                
+            print("alo", x_new, y_new)
+        
+            
+            """
+            """
+            
+            d_old = util.euclidean_distance(self.simulator.depot.coords, d)
+            
+            d_new = util.euclidean_distance(self.simulator.depot.coords, hello_packet.next_target)
+            
+            
+            
+            exp_position = hello_packet.cur_pos  # without estimation, a simple geographic approach
+            exp_distance = util.euclidean_distance(exp_position, self.simulator.depot.coords)
+            """
+            
+            import math
+            
+            final = hello_packet.next_target
+            
+            coords = hello_packet.cur_pos
+            
+            ang = math.atan2(final[1]-coords[1], final[0]-coords[0])
+            
+            angle = math.degrees(ang)
+            
+            if (angle < 0):
+                
+                angle = 360 + angle
+            
+            
+            
+            old_x, old_y = coords[0], coords[1]
+            angle = float(angle)
+            # Compute the change in position
+            
+            """
+            
+            speed = hello_packet.speed
+            delta_y = speed * math.cos(math.radians(angle))
+            delta_x = speed * math.sin(math.radians(angle))
+            # Add that to the existing position
+            new_x = old_x + delta_x 
+            new_y = old_y + delta_y 
+            """
+            
+            final_drone = self.drone.next_target()
+            
+            coords_drone = self.drone.coords
+            
+            ang_drone = math.atan2(final_drone[1]-coords_drone[1], final_drone[0]-coords_drone[0])
+            
+            angle_drone = math.degrees(ang_drone)
+            
+            if (angle_drone < 0):
+                
+                angle_drone = 360 + angle_drone
+            
+            
+            
+            angle_drone = float(angle_drone)
+            
+            
+            """
+            if (angle > 180):
+                print("who am i:", self.drone)
+                print("\tmine cur_pos:", self.drone.coords)
+                print("\t mine Next:", self.drone.next_target())
+                
+                print("\t mine angle: ", angle_drone)
+                print("i have this neighbour: ", drone_istance)
+                print("with this characteristics\n\t Cur_pos: ", hello_packet.cur_pos)
+                print("\t Next:", hello_packet.next_target)
+                
+                print("\t angle: ", angle)
+                #print(new_x, new_y)
+                
+                #if ()
+                
+                "input()"
+            """  
+            exp_position = hello_packet.cur_pos  # without estimation, a simple geographic approach
+            exp_distance = util.euclidean_distance(exp_position, self.simulator.depot.coords)
+            
+            
+            time_taken = exp_distance / hello_packet.speed
+            
+            
+            
+            #if (angle < 180 and angle_drone > 180):
+                
+             #   continue
+            
+            if exp_distance < best_drone_distance_from_depot:
+                best_drone_distance_from_depot = exp_distance
+                best_drone = drone_istance
+                time_taken_best = best_drone_distance_from_depot / hello_packet.speed
+                
+                
+            elif (best_drone != None and exp_distance >= best_drone_distance_from_depot and best_drone.next_target() == hello_packet.next_target):
+                
+                if (time_taken < time_taken_best):
+                    
+                    best_drone_distance_from_depot = exp_distance
+                    best_drone = drone_istance
+                    time_taken_best = best_drone_distance_from_depot / hello_packet.speed
+            
+            
+
+            
+            """
+            if exp_distance < best_drone_distance_from_depot or hello_packet.speed > self.drone.speed:
+                best_drone_distance_from_depot = exp_distance
+                best_drone = drone_istance
+            
+            """
+
+
+     
+
+        return best_drone
+
         # Only if you need --> several features:
         # cell_index = util.TraversedCells.coord_to_cell(size_cell=self.simulator.prob_size_cell,
         #                                                width_area=self.simulator.env_width,
